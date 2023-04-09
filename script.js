1482bdd02f5289bc0ca13f4ee265f3bbf6af7c4c
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-04-09 17:56:14 +0200

    Comentarios añadidos

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//  Aparece el div con la id de popup
+//  Cuando el boton del corto se pulsa
 function openPopup(VideoId) {
+    // Se muestra el div
     document.getElementById("popup").style.display = "block";
+    // Se cambia el src del iframe
     document.getElementById("IframeVideo").src = "https://www.youtube.com/embed/" + VideoId;
 }
 
-// Desaparece el div con la id de popup
+// Cuando el boton de cerrar se pulsa
 function closePopup() {
+    // Desaparece el div
     document.getElementById("popup").style.display = "none";
 }
