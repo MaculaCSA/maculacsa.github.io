ea96ef2d1b9787449daaf0b489d0800e18c950ae
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-04-09 18:03:01 +0200

    Cambios de optimización
    Ahora se carga antes de mostrarse

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 //  Cuando el boton del corto se pulsa
 function openPopup(VideoId) {
-    // Se muestra el div
-    document.getElementById("popup").style.display = "block";
     // Se cambia el src del iframe
     document.getElementById("IframeVideo").src = "https://www.youtube.com/embed/" + VideoId;
+    // Se muestra el div
+    document.getElementById("popup").style.display = "block";
 }
 
 // Cuando el boton de cerrar se pulsa
