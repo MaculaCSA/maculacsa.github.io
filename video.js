bf8ec830fa1e8c45ca506f35e424bbf6582fd90a
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-04-16 13:49:30 +0200

    Actualización del script

--- video.js
+++ video.js
@@ -1,23 +1,31 @@
+// obtener el elemento de video
 const video = document.getElementById("video");
-const videoHeight = 1080; // altura del video en píxeles
-const windowHeight = window.innerHeight; // altura de la ventana del navegador en píxeles
-const speed = 0.2; // velocidad de transición del video (0.2 significa que se actualizará un 20% del camino hacia el valor de destino en cada fotograma de animación)
 
-let targetTime = 0; // tiempo de destino para el video
+// función para manejar el evento de scroll
+function handleScroll() {
+  // obtener la altura de la ventana gráfica del navegador
+  const windowHeight = window.innerHeight;
 
-window.addEventListener("scroll", () => {
-  const scrollPosition = window.scrollY; // posición actual del scroll en píxeles
-  const videoPosition = scrollPosition / (document.body.clientHeight - windowHeight) * videoHeight;
-  // calcula la posición del video en relación con la posición del usuario en la página
-  targetTime = videoPosition;
-});
+  // obtener la duración total del video en segundos
+  const videoDuration = video.duration;
 
-function updateVideoTime() {
-  const currentTime = video.currentTime;
-  const diff = (targetTime - currentTime) * speed;
-  const nextTime = currentTime + diff;
-  video.currentTime = nextTime;
-  requestAnimationFrame(updateVideoTime);
+  // obtener la posición vertical actual de la página
+  const scrollPosition = window.scrollY;
+
+  // verificar si el valor de scrollPosition es mayor o igual a cero y si el valor de windowHeight es mayor que cero
+  if (scrollPosition >= 0 && windowHeight > 0) {
+    // calcular la posición del video en relación a la página
+    const videoPosition = scrollPosition / windowHeight;
+
+    // calcular el frame actual del video
+    const currentFrame = Math.round(videoPosition * videoDuration * 30); // 30 es la tasa de frames por segundo del video
+
+    // establecer el frame actual del video
+    video.currentTime = currentFrame / 30; // dividir por 30 para obtener el tiempo en segundos
+  }
 }
 
-requestAnimationFrame(updateVideoTime);
+// esperar a que la duración del video se cargue antes de vincular el evento scroll
+video.addEventListener("loadedmetadata", () => {
+  window.addEventListener("scroll", handleScroll);
+});
