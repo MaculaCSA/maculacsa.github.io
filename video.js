7b9802e1f7f0a667a387d82be2ae7ca7537122c0
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-04-16 14:22:14 +0200

    Script optimizado

--- video.js
+++ video.js
@@ -1,15 +1,22 @@
 const video = document.getElementById('video');
+let animationFrameId = null;
 
 function handleScroll() {
-  const windowHeight = window.innerHeight;
-  const pageHeight = document.body.scrollHeight;
-  const scrollPosition = window.scrollY;
-
-  if (scrollPosition >= 0 && windowHeight > 0 && pageHeight > 0) {
-    const totalTime = video.duration;
-    const currentTime = scrollPosition / (pageHeight - windowHeight) * totalTime;
-    video.currentTime = currentTime;
+  if (animationFrameId) {
+    cancelAnimationFrame(animationFrameId);
   }
+
+  animationFrameId = requestAnimationFrame(() => {
+    const windowHeight = window.innerHeight;
+    const pageHeight = document.body.scrollHeight;
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 0 && windowHeight > 0 && pageHeight > 0) {
+      const totalTime = video.duration;
+      const currentTime = scrollPosition / (pageHeight - windowHeight) * totalTime;
+      video.currentTime = currentTime;
+    }
+  });
 }
 
 video.addEventListener('loadedmetadata', () => {
