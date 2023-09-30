7bcb6d7d6a311234b050d2fbb0411c523c00cb07
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-09-30 21:01:30 +0000

    Modelo giratorio añadido

--- macula/src/App.js
+++ macula/src/App.js
@@ -11,8 +11,20 @@ function App() {
         <ParallaxLayer offset={2} style={{ backgroundColor: '#87BCDE' }} />
         
         <ParallaxLayer offset={0} speed={0.1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <h1>Sticky Layers!</h1>
+        <div id="content">
+          <h2>PREMIOS MÀCULA</h2>
+          <p>Cortos Agustinianos</p>
+          <button>Shop</button>
+        </div>
         </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div class="modelo">
+          <model-viewer id="aSide" src="model/maculanogirar.glb" alt="Macula" auto-rotate ar ar-modes="webxr scene-viewer quick-look" poster="model/poster.webp" shadow-intensity="1" autoplay environment-image="model/fireplace_2k.hdr"> </model-viewer>
+        </div>
+        </ParallaxLayer>
+
+
       </Parallax>
     
     </div>
