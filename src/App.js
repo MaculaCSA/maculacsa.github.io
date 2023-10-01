6809ee4bfc40537c26ce04bb267b04fca21a05a0
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-10-01 17:27:35 +0000

    2ª pantalla en blanco

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Parallax pages={10} scrolling={false}>
         <ParallaxLayer offset={0} style={{ backgroundColor: 'white' }} />
-        <ParallaxLayer offset={1} style={{ backgroundColor: '#805E73' }} />
+        <ParallaxLayer offset={1} style={{ backgroundColor: 'white' }} />
         <ParallaxLayer offset={2} style={{ backgroundColor: '#87BCDE' }} />
         
         <ParallaxLayer offset={0} speed={0.1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
