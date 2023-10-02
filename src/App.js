a14fffeccb61beb060d5987440b2969459410205
KOKO <126774488+KOKOSG7@users.noreply.github.com>
2023-10-02 17:05:15 +0000

    orden y cosas pequeñas arregladas

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ function App() {
     <div className="App">
       <Parallax pages={10} scrolling={false}>
         <ParallaxLayer offset={0}/>
-        <ParallaxLayer offset={1} style={{ backgroundColor: 'white' }} />
+        <ParallaxLayer offset={1}/>
         <ParallaxLayer offset={2} style={{ backgroundColor: '#87BCDE' }} />
         <ParallaxLayer offset={3} style={{ backgroundColor: '#87BCDE' }}/>
         
         <ParallaxLayer offset={0} speed={0.1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <div id="content">
-            <h2 class="titulo">PREMIOS MÀCULA</h2>
-            <div class="margen"></div>
+            <h2 class="titulo">PREMIOS MÁCULA</h2>
+
             <p class="subtitulo">2024</p>
           </div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} sticky={{}} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <ParallaxLayer offset={0} sticky={{}} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <div class="modelo">
             <model-viewer id="aSide" src="model/maculanogirar.glb" alt="Macula" auto-rotate loading="eager" ar ar-modes="webxr scene-viewer quick-look" poster="model/poster.webp" shadow-intensity="1" autoplay environment-image="model/fireplace_2k.hdr"> </model-viewer>
           </div>
@@ -34,7 +34,6 @@ function App() {
           <div id="content" class="creditos">
             <h3 class="titulo">Hecho con ❤️</h3>
             <h3 class="titulo">Pablo García y Aarón Sancibrián</h3>
-            <div class="margen"></div>
             <p class="subtitulo">Modelo por Hector Sancibrián</p>
           </div>
         </ParallaxLayer>
@@ -70,3 +69,4 @@ window.onload = function(){
   , 6000);
   
 }
+
