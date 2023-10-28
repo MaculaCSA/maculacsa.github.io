64619435537175f99d9b983d2906f3a39260a73d
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-10-28 16:03:48 +0000

    Foto de fondo

--- src/Sitio.jsx
+++ src/Sitio.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, react_app } from "react";
 
 import carga from './carga.js';
 
@@ -6,25 +6,30 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 import './Sitio.css';
 
-//fetch('archivo.json')
-//  .then(response => response.json())
-//  .then(data => {
-//    const variable1 = data.variable1;
-//    const variable2 = data['variable2'];
-    // Usa las variables como necesites
-//  });
+const datos = require('./datos.json');
 
 const Sitios = ({ciudad}) => {
+
+    console.log(ciudad);
+    //coger dato de datos.json
+    const datosCiudad = datos[ciudad];
+    const titulo = datosCiudad.titulo;
+    const fotociudad = '/img/colegios/' + datosCiudad.fotociudad;
+    
+    console.log(datosCiudad);
+
     window.onload = carga()
     return (
         <div className="App">
       <Parallax pages={3} scrolling={false}>
-        <ParallaxLayer offset={2} style={{backgroundImage: `url(${ciudad})`}} />
+        <ParallaxLayer offset={0}>
+          <img className="fondo" src={fotociudad} alt="" />
+        </ParallaxLayer>
 
 
         <ParallaxLayer offset={0} speed={0.1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <div id="content">
-            <h2 class="titulo titulociudad">{ciudad}</h2>
+            <h2 class="titulo titulociudad">{titulo}</h2>
 
             <p class="subtitulo">Mácula 2024</p>
           </div>
