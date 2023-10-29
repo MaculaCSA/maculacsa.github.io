34627929ebb42afc54e2b3488158c8e9e0c4d005
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-10-29 20:08:17 +0000

    Primera página empezada

--- src/Sitio.jsx
+++ src/Sitio.jsx
@@ -15,13 +15,15 @@ const Sitios = ({ciudad}) => {
     const datosCiudad = datos[ciudad];
     const titulo = datosCiudad.titulo;
     const fotociudad = '/img/colegios/' + datosCiudad.fotociudad;
+
+    const modelocategoria = '/img/categorias/' + 'claqueta.mp4';
     
     console.log(datosCiudad);
 
     window.onload = carga()
     return (
         <div className="App">
-      <Parallax pages={3} scrolling={false}>
+      <Parallax style={{ backgroundColor: '#212121' }} pages={2} scrolling={false}>
         <ParallaxLayer offset={0}>
           <img className="fondo" src={fotociudad} alt="" />
         </ParallaxLayer>
@@ -33,6 +35,24 @@ const Sitios = ({ciudad}) => {
 
             <p className="subtitulo">Mácula 2024</p>
           </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <video src={modelocategoria} className="modelocategoria" autoPlay muted loop />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          
+        <div className="item categoria">
+          <p className="info">Nominados al mejor guion:</p>
+          <div className="container2">
+            <button style={{backgroundImage: 'url(img/nominados/rencor.png)'}} onClick={() => console.log('GTxQBbB46mo')} className="corto fondoimg"><p className="nombre">El Rencor</p></button>
+            <button style={{backgroundImage: 'url(img/nominados/asalto.png)'}} onClick={() => console.log('xat-XYdr_u8')} className="corto fondoimg ganador"><p className="nombre">Un Asalto a la historia</p></button>
+            <button style={{backgroundImage: 'url(img/nominados/x.png)'}} onClick={() => console.log('pSFSd89KPbc')} className="corto fondoimg"><p className="nombre">Proyecto X</p></button>
+            <button style={{backgroundImage: 'url(img/nominados/newton2.png)'}} onClick={() => console.log('i7XQBbb94pY')} className="corto fondoimg"><p className="nombre">Newton</p></button>
+          </div>
+        </div>
+          
         </ParallaxLayer>
 
       </Parallax>
