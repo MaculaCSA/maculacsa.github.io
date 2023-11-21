6bacbfd681ca9b88904b4e209e9832d329925ae5
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-11-21 21:19:00 +0000

    Popup añadido

--- src/Sitio.jsx
+++ src/Sitio.jsx
@@ -36,7 +36,7 @@ const Sitios = ({ciudad}) => {
     const cortos = datosCiudad.categorias[categoria].map((corto) => (
       <button
         style={{backgroundImage: `url(img/nominados/${corto.nombre_foto})`}}
-        onClick={() => console.log(corto.youtube_id)}
+        onClick={() => openPopup(corto.youtube_id)}
         className="corto fondoimg"
       >
         <p className="nombre">{corto.titulo}</p>
@@ -60,6 +60,34 @@ const Sitios = ({ciudad}) => {
   return (
     <div className="App">
 
+
+
+<button id="closePopup" className="cerrarpopupgigante" onClick={closePopup}></button>
+
+      <div className="popup" id="popup">
+        <iframe
+          width="100%"
+          height="100%"
+          id="IframeVideo"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <button className="cerrarpopup" onClick={closePopup}>
+          <img src="../img/x-square.svg" alt="Cerrar" />
+        </button>
+      </div>
+
+      <div className="popup" id="popupImg">
+        <img src="img/logoMP.png" id="IframeImg" frameBorder="0" alt="" />
+        <button className="cerrarpopup cerrarpopupImg" onClick={closePopup}>
+          <img src="../img/x-square.svg" alt="Cerrar" />
+        </button>
+      </div>
+
+
+
       <Parallax style={{ backgroundColor: '#212121' }} pages={numPaginas} scrolling={false}>
         <ParallaxLayer offset={0}>
           <img className="fondo" src={fotociudad} alt="" />
@@ -81,9 +109,48 @@ const Sitios = ({ciudad}) => {
         {categorias}
 
       </Parallax>
-    
     </div>
   );
 };
  
+const userAgent = navigator.userAgent;
+
+function openPopup(VideoId) {
+  // Si el usuario es de movil, en vez de ejecutarse close popup se redirige a la pagina de youtube
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+      url("https://www.youtube.com/watch?v=" + VideoId);
+  }
+  else {
+      // Se cambia el src del iframe
+      document.getElementById("IframeVideo").src = "https://www.youtube.com/embed/" + VideoId;
+      // Se muestra el div
+      document.getElementById("popup").style.display = "block";
+      document.getElementById("closePopup").style.display = "block";
+  }
+}
+
+// Abrir popup para carteles
+function openPopupImg(ImageId) {
+  // Se cambia el src del iframe
+  document.getElementById("IframeImg").src = "img/nominados/carteles/" + ImageId;
+  // Se muestra el div
+  document.getElementById("popupImg").style.display = "block";
+  document.getElementById("closePopup").style.display = "block";
+
+}
+
+// Cuando el boton de cerrar se pulsa
+function closePopup() {
+  // Desaparece el div
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("popupImg").style.display = "none";
+  document.getElementById("closePopup").style.display = "none";
+}
+
+// Redirigir a una web
+function url(url) {
+  window.open(url);
+}
+
+
 export default Sitios;
