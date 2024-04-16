f7542240f066b60034b39dcb89a85c455c5a578e
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2024-04-16 20:05:09 +0000

    Variable homepage eliminada (Es el directorio raiz)

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import Sitios from './Sitio';
-
 import ErrorPage from './errorpage';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-var ReactDOMClient = require('react-dom/client');
 
+import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOMClient.createRoot(document.getElementById("root"));
+const router = createHashRouter([
+  { path: '/', element: <App /> },
+  { path: '/nominados', element: <Sitios ciudad={"nominados"} /> },
+  { path: '*', element: <ErrorPage /> },
+]);
 
-root.render(
-  // <React.StrictMode>
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/nominados" element={<Sitios ciudad="nominados" />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  </Router>
-  // </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 reportWebVitals();
