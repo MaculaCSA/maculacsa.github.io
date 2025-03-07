8adbd7cee61d8d3e2b9cb55897eece7a8cbeedad
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2025-03-07 23:38:33 +0000

    Agregar importación de datos JSON y generar rutas dinámicas por año en index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Sitios from './Sitio';
-
+import importarTodosLosDatos from './importarDatos.js';
 import ErrorPage from './errorpage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Importar todos los datos de los archivos JSON
+const datosPorAno = importarTodosLosDatos();
+console.log("index.js - Archivos JSON encontrados:", Object.keys(datosPorAno));
+
+// Crear rutas dinámicamente para cada año
+const generarRutasPorAno = () => {
+  return Object.keys(datosPorAno).map(ano => (
+    <Route key={ano} path={`/${ano}`} element={<Sitios ciudad="nominados" ano={ano} />} />
+  ));
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +26,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/nominados" element={<Sitios ciudad="nominados" ano="2024"/>} />
+        {generarRutasPorAno()}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
